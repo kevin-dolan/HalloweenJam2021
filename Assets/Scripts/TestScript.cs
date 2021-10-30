679e71526ee67609e39cd9042adac05f4188c996
Joseph Hong <jsh7099@rit.edu>
2021-10-29 22:12:34 -0400

    joe comment added

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Joe was here
     }
 
     // Update is called once per frame
