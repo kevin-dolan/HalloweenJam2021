fe95eec508051eb18da1aff293456fe0d739b585
Kevin Dolan <kevindolansomewhere@gmail.com>
2021-10-30 23:23:00 -0400

    Spawn system is working better, needs a Queue system next

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -8,9 +8,13 @@ public class EnemyManager : MonoBehaviour
 
     [SerializeField] private Sprite[] enemySprites = new Sprite[4]; ////MUST BE SET IN INSPECTOR
     [SerializeField] private GameObject enemyPrefab; //MUST BE SET IN INSPECTOR
+    [SerializeField] private List<EnemyGroup> eGroups = new List<EnemyGroup>(); //list of groups of enemies to spawn in the level this EnemyManager is currently in.
+
+    private float spawnDelayCounter = 0;
 
     private Vector3 spawnPosition; //used while instantiating enemies. manipulated to spawn enemies in patterns.
 
+    private EnemyGroup enemyGroupHolder;
     private GameObject enemyHolder;
 
     // Start is called before the first frame update
@@ -23,19 +27,20 @@ public class EnemyManager : MonoBehaviour
         //GameObject enemy = Instantiate(enemyPrefab, new Vector3(10.0f, 0.0f, 0.0f), Quaternion.identity);
         //enemy.GetComponent<EnemyBehavior>().SetBehavior(2, 1.0f, MovePattern.Zigzag, ShootPattern.None);
 
-        GameObject enemy2 = Instantiate(enemyPrefab, new Vector3(10.0f, 2.0f, 0.0f), Quaternion.identity);
-        enemy2.GetComponent<SpriteRenderer>().sprite = enemySprites[1];
-        enemy2.GetComponent<EnemyBehavior>().SetBehavior(2, 2.5f, MovePattern.Sine);
+        //GameObject enemy2 = Instantiate(enemyPrefab, new Vector3(10.0f, 2.0f, 0.0f), Quaternion.identity);
+        //enemy2.GetComponent<SpriteRenderer>().sprite = enemySprites[1];
+        //enemy2.GetComponent<EnemyBehavior>().SetBehavior(2, 2.5f, MovePattern.Sine);
 
-        GameObject enemy3 = Instantiate(enemyPrefab, new Vector3(10.0f, -2.0f, 0.0f), Quaternion.identity);
-        enemy3.GetComponent<EnemyBehavior>().SetBehavior(2, 2.0f, MovePattern.Stutter);
+        //GameObject enemy3 = Instantiate(enemyPrefab, new Vector3(10.0f, -2.0f, 0.0f), Quaternion.identity);
+        //enemy3.GetComponent<EnemyBehavior>().SetBehavior(2, 2.0f, MovePattern.Stutter);
 
-        for(int i = 0; i < 5; i++)
-        {
-            enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, 0.0f + (i * 0.75f), 0.0f), Quaternion.identity);
-            enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(2, 1.0f, MovePattern.Zigzag);
-        }
+        //for(int i = 0; i < 5; i++)
+        //{
+        //    enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, 0.0f + (i * 0.75f), 0.0f), Quaternion.identity);
+        //    enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(2, 1.0f, MovePattern.Zigzag);
+        //}
 
+        
 
 
         //enemy.GetComponent<EnemyBehavior>()
@@ -50,6 +55,21 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnDelayCounter <= 0) //spawn the next wave when spawnDelayCounter is 0 or less
+        {
+            for (int i = 0; i < eGroups[0].numOfEnemies; i++)
+            {
+                enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, eGroups[0].initialHeight + (0.75f * i), 0.0f), Quaternion.identity);
+                enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(eGroups[0].enemyHealth,
+                                                                      eGroups[0].enemySpeed,
+                                                                      eGroups[0].movePattern,
+                                                                      eGroups[0].shootPattern,
+                                                                      eGroups[0].shootCooldown);
+            }
+
+            spawnDelayCounter = eGroups[0].spawnDelay; //set the delay
+
+        }
+        spawnDelayCounter -= Time.deltaTime;
     }
 }
