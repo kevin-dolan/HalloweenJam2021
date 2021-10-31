5b5afa105bfb94fce0eb6ffd5f691b55e14b1fd6
Joseph Hong <jsh7099@rit.edu>
2021-10-30 21:36:47 -0400

    Main work wip

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float bulletCoolDown;
+    //[SerializeField] private float bulletSpeed;
+    [SerializeField] private int damage;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float invincibilityCoolDOwn;
+    private float cooldownCounter;
+    [SerializeField] private int playerHealth;
+    private GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerSpeed = gameObject.GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player presses W or Up key
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 playerPosition = transform.position;
+            playerPosition.y += playerSpeed;
+            transform.position = playerPosition;
+        }
+
+        //if the player presses S or Down key
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 playerPosition = transform.position;
+            playerPosition.y -= playerSpeed;
+            transform.position = playerPosition;
+        }
+
+        //if the player presses W or Up key
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 playerPosition = transform.position;
+            playerPosition.x += playerSpeed;
+            transform.position = playerPosition;
+        }
+
+        //if the player presses A or Left key
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 playerPosition = transform.position;
+            playerPosition.x -= playerSpeed;
+            transform.position = playerPosition;
+        }
+
+        //cooling down based on real time
+        if (cooldownCounter > 0)
+        {
+            cooldownCounter -= Time.deltaTime;
+
+        }
+
+        //pressing space to shoot
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cooldownCounter > 0)
+            {
+                return;
+            }
+            Debug.Log("Shoot");
+            Shoot();
+            cooldownCounter = bulletCoolDown;
+        }
+
+        //takeDamage();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //cooling down based on real time
+        if (cooldownCounter > 0)
+        {
+            cooldownCounter -= Time.deltaTime;
+
+        }
+
+        if (collision.gameObject.tag == "enemy")
+        {
+            Debug.Log(playerHealth);
+            if (cooldownCounter > 0)
+            {
+                Debug.Log("im not hurt");
+                return;
+            }
+            else
+            {
+                Debug.Log("im hurt");
+                playerHealth -= 1;
+            }
+            cooldownCounter = invincibilityCoolDOwn;
+            //game over when the player's health is 0
+            if (playerHealth == 0)
+            {
+                Debug.Log("You died.");
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.GetComponent<BulletScript>().dmg = damage;
+    }
+        
+}
