b74ef298d3b5966587d097260ea5497aca08c1c1
Kevin Dolan <kevindolansomewhere@gmail.com>
2021-10-31 10:18:38 -0400

    Enemy spawning system is complete.
    Levels will need to have their list of groups of enemies to spawn manually designed in the inspector per level.

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
 
-
-    [SerializeField] private Sprite[] enemySprites = new Sprite[4]; ////MUST BE SET IN INSPECTOR
     [SerializeField] private GameObject enemyPrefab; //MUST BE SET IN INSPECTOR
-    [SerializeField] private List<EnemyGroup> eGroups = new List<EnemyGroup>(); //list of groups of enemies to spawn in the level this EnemyManager is currently in.
+    [SerializeField] private List<EnemyGroup> eGroupsList = new List<EnemyGroup>(); //list of groups of enemies to spawn in the level this EnemyManager is currently in.
+    [SerializeField] private Queue<EnemyGroup> eGroups = new Queue<EnemyGroup>(); //QUEUE of groups of enemies
 
-    private float spawnDelayCounter = 0;
+    private float spawnDelayCounter = 3.0f; //used to delay between spawns of enemy groups in FixedUpdate()
 
     private Vector3 spawnPosition; //used while instantiating enemies. manipulated to spawn enemies in patterns.
 
@@ -20,31 +19,10 @@ public class EnemyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //spawn between y (3, -4.5)
-
-        //EnemyBehavior enemy = new EnemyBehavior(10, 10.0f, MovePattern.Straight, ShootPattern.Straight, enemySprites[0]); 
-
-        //GameObject enemy = Instantiate(enemyPrefab, new Vector3(10.0f, 0.0f, 0.0f), Quaternion.identity);
-        //enemy.GetComponent<EnemyBehavior>().SetBehavior(2, 1.0f, MovePattern.Zigzag, ShootPattern.None);
-
-        //GameObject enemy2 = Instantiate(enemyPrefab, new Vector3(10.0f, 2.0f, 0.0f), Quaternion.identity);
-        //enemy2.GetComponent<SpriteRenderer>().sprite = enemySprites[1];
-        //enemy2.GetComponent<EnemyBehavior>().SetBehavior(2, 2.5f, MovePattern.Sine);
-
-        //GameObject enemy3 = Instantiate(enemyPrefab, new Vector3(10.0f, -2.0f, 0.0f), Quaternion.identity);
-        //enemy3.GetComponent<EnemyBehavior>().SetBehavior(2, 2.0f, MovePattern.Stutter);
-
-        //for(int i = 0; i < 5; i++)
-        //{
-        //    enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, 0.0f + (i * 0.75f), 0.0f), Quaternion.identity);
-        //    enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(2, 1.0f, MovePattern.Zigzag);
-        //}
-
-        
-
-
-        //enemy.GetComponent<EnemyBehavior>()
-
+        foreach(EnemyGroup group in eGroupsList) //at the start of the scene, add all the enemy groups from eGroupsList to the queue eGroups.
+        {
+            eGroups.Enqueue(group);
+        }
     }
 
     private void SpawnGroup(int numOfEnemies, Vector3 initialPosition, MovePattern mp, ShootPattern sp)
@@ -53,22 +31,30 @@ public class EnemyManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (spawnDelayCounter <= 0) //spawn the next wave when spawnDelayCounter is 0 or less
         {
-            for (int i = 0; i < eGroups[0].numOfEnemies; i++)
+            //THIS IS BASICALLY THE LEVEL COMPLETE CONDITION!
+            if(eGroups.Count <= 0) //if eGroups is empty, just do nothing FOR NOW
             {
-                enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, eGroups[0].initialHeight + (0.75f * i), 0.0f), Quaternion.identity);
-                enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(eGroups[0].enemyHealth,
-                                                                      eGroups[0].enemySpeed,
-                                                                      eGroups[0].movePattern,
-                                                                      eGroups[0].shootPattern,
-                                                                      eGroups[0].shootCooldown);
+                Debug.Log("No more groups to spawn!");
+                spawnDelayCounter = 999.0f;
+                return;
             }
 
-            spawnDelayCounter = eGroups[0].spawnDelay; //set the delay
+            enemyGroupHolder = eGroups.Dequeue(); //take the next enemyGroup from eGroups and spawn it.
+            for (int i = 0; i < enemyGroupHolder.numOfEnemies; i++)
+            {
+                enemyHolder = Instantiate(enemyPrefab, new Vector3(10.0f, enemyGroupHolder.initialHeight + (0.75f * i), 0.0f), Quaternion.identity);
+                enemyHolder.GetComponent<EnemyBehavior>().SetBehavior(enemyGroupHolder.enemyHealth,
+                                                                      enemyGroupHolder.enemySpeed,
+                                                                      enemyGroupHolder.movePattern,
+                                                                      enemyGroupHolder.shootPattern,
+                                                                      enemyGroupHolder.shootCooldown);
+            }
 
+            spawnDelayCounter = enemyGroupHolder.spawnDelay; //set the delay until the next group is spawned.
         }
         spawnDelayCounter -= Time.deltaTime;
     }
